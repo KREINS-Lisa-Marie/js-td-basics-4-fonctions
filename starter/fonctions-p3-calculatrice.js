546ce19e69cabcf814c8cdd74005672bff1e67ba
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2024-10-20 10:51:19 +0200

    commit 3  20.10.24

--- starter/fonctions-p3-calculatrice.js
+++ starter/fonctions-p3-calculatrice.js
@@ -14,6 +14,23 @@ Aide : votre fonction s'exécute avec le pattern suivant : calculer(nb1, "+", nb
 
 // Déclaration de la fonction calculer
 
+function calculer(nb1,operateur,nb2) {
+switch (operateur) {
+    case "+":
+        return nb1 + nb2;
+    case "-":
+        return nb1 - nb2;
+    case "*":
+        return nb1 * nb2;
+    case "/":
+        return nb1 / nb2;
+}
+}
+
 
 // Utilisation de la fonction calculer
 
+console.log(calculer(4, "+", 6));
+console.log(calculer(4, "-", 6));
+console.log(calculer(2, "*", 0));
+console.log(calculer(12, "/", 0));
